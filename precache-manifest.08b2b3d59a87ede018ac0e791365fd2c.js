8378b161eb9cafd0f71c41c5e7649c169e2e9fb8
aksel26 <kevinxkim2023@gmail.com>
2020-01-25 21:31:11 +0900

    Updates

---
+++ precache-manifest.08b2b3d59a87ede018ac0e791365fd2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109247793703de3fb326634e60c806f3",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7c1a2349f1474e905de8",
+    "url": "/movie_app_2020/static/css/main.e7ad7e59.chunk.css"
+  },
+  {
+    "revision": "3fdb48135cca83b8334b",
+    "url": "/movie_app_2020/static/js/2.d3a28558.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.d3a28558.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c1a2349f1474e905de8",
+    "url": "/movie_app_2020/static/js/main.1e4dc6bf.chunk.js"
+  },
+  {
+    "revision": "0e11ddbc603fda3de7ed",
+    "url": "/movie_app_2020/static/js/runtime-main.4984633c.js"
+  }
+]);
