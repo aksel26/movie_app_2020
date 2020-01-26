a17b8827dc4b030f245e73f52a20a0052b99f87a
aksel26 <kevinxkim2023@gmail.com>
2020-01-26 15:46:56 +0900

    redirect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
-function Detail(props){
-    console.log(props)
-    ;
-    return <span> hello</span>
+class Detail extends React.Component{
+    componentDidMount(){
+        console.log(this.props)
+        const {location,history} = this.props;
+        
+        // undefined상태
+        // Redirect해주기
+        if (location.state===undefined){
+            console.log(this.props)
+                history.push("/");
+        }
+    }
+    render(){
+        const{location} = this.props;
+        if(location.state){
+            return <span>{location.state.title}</span>
+        }else{
+        return null;
+        }
+    }
 }
-
 export default Detail;
