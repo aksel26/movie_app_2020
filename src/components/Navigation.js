4b9ecfe0415144162eae7dddc66f99c2c95f45f3
aksel26 <kevinxkim2023@gmail.com>
2020-01-26 14:44:59 +0900

    Link

---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Link} from "react-router-dom"
+function Navigation(){
+    return (
+    
+
+    // 페이지 이동이나 새로고침 하면 리액트를 죽이고 새로 하기 때ㅐ문에 비효율적
+    // Link를 이용
+    // Link를 쓰려면 App.js에서 HashRouter 안에 Navigation이 위치해야 한다.
+    
+    // <div>
+    //     <a href="/"> Home</a>
+    //     <a href="/about">About</a>
+    // </div>
+
+    <div>
+        <Link to="/"> Home</Link>
+        <Link to="/about">About</Link>
+    </div>
+    )
+}
+
+
+export default Navigation
---
+++ src/routes/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function About (){
+    return <span>
+
+        About this page : i built it becuz i like movies
+    </span>
+}
+
+export default About;
